79eb09559175abaed1d95e74c145a275b8a5eda0
Arthur Torres <54257895+NamelessMiojo@users.noreply.github.com>
2021-05-26 20:27:11 -0300

    Criação do projeto

---
+++ src/main/java/dominio/Turma.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dominio;
+
+import enumerator.eModEnsino;
+import java.util.List;
+
+/**
+ *
+ * @author Arthur
+ */
+public class Turma {
+    
+    private int nome;
+    private eModEnsino ensino;
+    private int ano;
+    private int totalAlunos;
+    
+
+}
---
+++ src/main/java/enumerator/eModEnsino.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package enumerator;
+
+/**
+ *
+ * @author Arthur
+ */
+public enum eModEnsino {
+    FUNDAMENTAL("Fundamental"),
+    FUNDAMENTAL_I("Fundamental I"),
+    MEDIO("Médio");
+    
+    private String descricao;
+
+    private eModEnsino(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+    
+}
